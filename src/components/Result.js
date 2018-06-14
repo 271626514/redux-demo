32101b7c36c72b0ba4f4321b03927da898cf5663
qiankun10271 <qiankun10271@autohome.com.cn>
2018-06-14 18:27:00 +0800

    up

--- src/components/Result.js
+++ src/components/Result.js
@@ -7,16 +7,14 @@ import {Progress} from 'antd';
 class Result extends React.Component {
   render() {
     return <div className='Result'>
-      <h3>下面的计数器会add和deltet按钮操作发生变化</h3>
+      <h3>{this.props.msg}</h3>
       <Progress style={{'margin':'0 auto',width: '120px', height: '120px'}} percent={this.props.count} type="circle" />
     </div>
   }
 }
 
 function mapStateToProps (state) {
-  return {
-    count: state
-  }
+  return state
 }
 
 export default connect(mapStateToProps)(Result)
--- src/index.js
+++ src/index.js
@@ -3,22 +3,35 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import DevTools from './reduxDevTools'
+
 import registerServiceWorker from './registerServiceWorker';
 
-function reducer (state=50, action) {
+const initState = {
+  count: 50,
+  msg: '请点击上面的两个按钮试试看!'
+}
+
+function reducer (state, action) {
   switch (action.type) {
     case 'ADD':
-      return state + 10
+      return {
+        count: state.count + 10,
+        msg: '您点击了ADD按钮'
+      }
     case 'DELTET':
-      return state - 10
+      return {
+        count: state.count - 10,
+        msg: '您点击了Delete按钮'
+      }
     default:
       return state
   }
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, initState, DevTools.instrument())
 
 console.log(store)
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><div><App /></div></Provider>, document.getElementById('root'));
 registerServiceWorker();
